817fe89ab29088392c85c0a604f7ba5d8f2e25f7
Anish <xevenbiswas@gmail.com>
2024-04-09 23:10:45 +0530

    added hero and etc

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { MenuIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="bg-gray-950">
+    <header className="dark:bg-background border-b">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="md:flex md:items-center md:gap-12">
-            <Link className="block text-teal-600" href="/">
+            <Link className="block text-primary" href="/">
               <span className="sr-only">Home</span>
               <Image src="/blox.webp" alt="" width={30} height={30} />
             </Link>
@@ -18,7 +20,7 @@ const Header = () => {
               <ul className="flex items-center gap-6 text-sm">
                 <li>
                   <Link
-                    className="transition text-white hover:text-white/75"
+                    className="transition text-foreground hover:text-foreground/75"
                     href="/"
                   >
                     About
@@ -27,7 +29,7 @@ const Header = () => {
 
                 <li>
                   <Link
-                    className="transition text-white hover:text-white/75"
+                    className="transition text-foreground hover:text-foreground/75"
                     href="/"
                   >
                     Careers
@@ -36,7 +38,7 @@ const Header = () => {
 
                 <li>
                   <Link
-                    className="transition text-white hover:text-white/75"
+                    className="transition text-foreground hover:text-foreground/75"
                     href="/"
                   >
                     History
@@ -45,7 +47,7 @@ const Header = () => {
 
                 <li>
                   <Link
-                    className="transition text-white hover:text-white/75"
+                    className="transition text-foreground hover:text-foreground/75"
                     href="/"
                   >
                     Services
@@ -54,7 +56,7 @@ const Header = () => {
 
                 <li>
                   <Link
-                    className="transition text-white hover:text-white/75"
+                    className="transition text-foreground hover:text-foreground/75"
                     href="/"
                   >
                     Projects
@@ -63,7 +65,7 @@ const Header = () => {
 
                 <li>
                   <Link
-                    className="transition text-white hover:text-white/75"
+                    className="transition text-foreground hover:text-foreground/75"
                     href="/"
                   >
                     Blog
@@ -75,40 +77,21 @@ const Header = () => {
 
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
-              <Link
-                className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow hover:bg-teal-500"
-                href="/"
-              >
-                Login
-              </Link>
+              <Button>
+                <Link href="/">Login</Link>
+              </Button>
 
               <div className="hidden sm:flex">
-                <Link
-                  className="rounded-md px-5 py-2.5 text-sm font-medium bg-gray-800 text-white hover:text-white/75"
-                  href="/"
-                >
-                  Register
-                </Link>
+                <Button variant="secondary">
+                  <Link href="/">Sign Up</Link>
+                </Button>
               </div>
             </div>
 
             <div className="block md:hidden">
-              <button className="rounded p-2 transition bg-gray-800 text-white hover:text-white/75">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
+              <Button variant="secondary" className="p-2">
+                <MenuIcon size={24} />
+              </Button>
             </div>
           </div>
         </div>
---
+++ src/components/ui/scroll-anim.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useRef } from "react";
+import { useScroll, useTransform, motion, MotionValue } from "framer-motion";
+
+export const ContainerScroll = ({
+  titleComponent,
+  children,
+}: {
+  titleComponent: string | React.ReactNode;
+  children: React.ReactNode;
+}) => {
+  const containerRef = useRef<any>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+  });
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  React.useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
+  const scaleDimensions = () => {
+    return isMobile ? [0.7, 0.9] : [1.05, 1];
+  };
+
+  const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
+  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
+  const translate = useTransform(scrollYProgress, [0, 1], [0, -100]);
+
+  return (
+    <div
+      className="h-[60rem] md:min-h-svh flex justify-center relative py-4 px-2 md:px-20"
+      ref={containerRef}
+    >
+      <div
+        className="py-10 md:py-24 w-full relative"
+        style={{
+          perspective: "1000px",
+        }}
+      >
+        <Header translate={translate} titleComponent={titleComponent} />
+        <Card rotate={rotate} translate={translate} scale={scale}>
+          {children}
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export const Header = ({ translate, titleComponent }: any) => {
+  return (
+    <motion.div
+      style={{
+        translateY: translate,
+      }}
+      className="div max-w-5xl mx-auto text-center"
+    >
+      {titleComponent}
+    </motion.div>
+  );
+};
+
+export const Card = ({
+  rotate,
+  scale,
+  children,
+}: {
+  rotate: MotionValue<number>;
+  scale: MotionValue<number>;
+  translate: MotionValue<number>;
+  children: React.ReactNode;
+}) => {
+  return (
+    <motion.div
+      style={{
+        rotateX: rotate,
+        scale,
+        boxShadow:
+          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
+      }}
+      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full p-2 md:p-6 bg-stone-300 dark:bg-stone-950 rounded-[30px] shadow-2xl"
+    >
+      <div className=" h-full w-full  overflow-hidden rounded-2xl bg-stone-100 dark:bg-neutral-900 md:rounded-2xl md:p-4">
+        {children}
+      </div>
+    </motion.div>
+  );
+};
