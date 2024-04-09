a82b222bad90ce65707a8ffea475600ddbf77ebc
Anish <xevenbiswas@gmail.com>
2024-04-10 00:08:46 +0530

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-const poppins = Poppins({ weight: ["400", "700", "600"] });
+const poppins = Poppins({ weight: ["400", "700", "600"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Blox AI",
