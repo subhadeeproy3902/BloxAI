a0649811e2ad416ec12d4a022788b19dfaec35e1
Anish <xevenbiswas@gmail.com>
2024-04-11 00:17:27 +0530

    1.14 added convex

---
+++ src/app/dashboard/_components/Header.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { Search, Send } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+function Header() {
+  const { user }: any = useKindeBrowserClient();
+  return (
+    <div className="flex justify-end w-full gap-2 items-center">
+      <div className="flex gap-2 items-center border rounded-md p-1">
+        <Search className="h-4 w-4" />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div>
+        <Image
+          src={user?.picture}
+          alt="user"
+          width={30}
+          height={30}
+          className="rounded-full"
+        />
+      </div>
+      <Button className="gap-2 flex text-sm h-8 hover:bg-blue-700 bg-blue-600">
+        <Send className="h-4 w-4" /> Invite
+      </Button>
+    </div>
+  );
+}
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import ConvexClientProvider from "./ConvexClientProvider";
 const poppins = Poppins({ weight: ["400", "700", "600"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
