931040ead5dc92d16471554d305f8e1da66b6cde
Anish <xevenbiswas@gmail.com>
2024-04-13 22:07:36 +0530

    add tooltips section

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,12 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
       },
       {
-        protocol:"https",
-        hostname:"picsum.photos",
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       }
     ]
   }
