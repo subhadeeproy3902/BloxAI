b72d2a36ddd8cbaaa4c49e13f0b1c8ee2b9800ed
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-04-26 00:29:56 +0530

    tried mermaid to excalidraw

--- src/app/workspace/_components/Canvas.tsx
+++ src/app/workspace/_components/Canvas.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
+import {
+  Excalidraw,
+  MainMenu,
+  WelcomeScreen,
+  convertToExcalidrawElements,
+} from "@excalidraw/excalidraw";
 import { FILE } from "../../dashboard/_components/FileList";
+import { parseMermaidToExcalidraw } from "@excalidraw/mermaid-to-excalidraw";
 import { useMutation } from "convex/react";
 import { useTheme } from "next-themes";
 import { api } from "../../../../convex/_generated/api";
 import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
 
 function Canvas({
   onSaveTrigger,
@@ -17,7 +24,6 @@ function Canvas({
 }) {
   const [whiteBoardData, setWhiteBoardData] = useState<any>();
   const { theme } = useTheme();
-  console.log(theme);
 
   const updateWhiteboard = useMutation(api.files.updateWhiteboard);
   useEffect(() => {
@@ -27,49 +33,90 @@ function Canvas({
     updateWhiteboard({
       _id: fileId,
       whiteboard: JSON.stringify(whiteBoardData),
-    })
-      .catch((e) => {
-        toast.error("Failed to save whiteboard!");
-      });
+    }).catch((e) => {
+      toast.error("Failed to save whiteboard!");
+    });
   };
+
+  const [mermaidCode, setMermaidCode] = useState<string>("");
+
+  const handleMermaidToExcalidraw = async () => {
+    const mermaidCode = `flowchart TD
+    A[Christmas] -->|Get money| B(Go shopping)
+    B --> C{Let me think}
+    C -->|One| D[Laptop]
+    C -->|Two| E[iPhone]
+    C -->|Three| F[Car]
+    `;
+
+    try {
+      const { elements } = await parseMermaidToExcalidraw(mermaidCode, {
+        fontSize: 16, // Example font size
+      });
+
+      console.log("Success:", elements);
+
+      // Set the converted elements to the state
+      setWhiteBoardData(elements);
+    } catch (e) {
+      console.error("Failed to parse Mermaid syntax:", e);
+    }
+  };
+
+  console.log("Whiteboard data", whiteBoardData);
+
   return (
-    <div style={{ height: "670px" }}>
-      {fileData && (
-        <Excalidraw
-          theme={theme === "dark" ? "dark" : "light"}
-          initialData={{
-            elements: fileData?.whiteboard && JSON.parse(fileData?.whiteboard),
-          }}
-          onChange={(excalidrawElements, appState, files) =>
-            setWhiteBoardData(excalidrawElements)
-          }
-          UIOptions={{
-            canvasActions: {
-              changeViewBackgroundColor: true,
-              saveToActiveFile: true,
-              loadScene: true,
-              export: { saveFileToDisk: true },
-              toggleTheme: true,
-              saveAsImage: true,
-            },
-          }}
-        >
-          <MainMenu>
-            <MainMenu.DefaultItems.ClearCanvas />
-            <MainMenu.DefaultItems.SaveAsImage />
-            <MainMenu.DefaultItems.ChangeCanvasBackground />
-          </MainMenu>
-          <WelcomeScreen>
-            <WelcomeScreen.Hints.MenuHint />
-            <WelcomeScreen.Hints.MenuHint />
-            <WelcomeScreen.Hints.ToolbarHint />
-            <WelcomeScreen.Center>
-              <WelcomeScreen.Center.MenuItemHelp />
-            </WelcomeScreen.Center>
-          </WelcomeScreen>
-        </Excalidraw>
-      )}
-    </div>
+    <>
+      <Button onClick={handleMermaidToExcalidraw}>Prompt</Button>
+      <div style={{ height: "670px" }}>
+        {fileData && (
+          <>
+            {/*Render the whiteboard data too if the mermaid code is converted to excalidraw elements*/}
+
+            <Excalidraw
+              theme={theme === "dark" ? "dark" : "light"}
+              initialData={{
+                elements:
+                  whiteBoardData?.length > 0
+                    ? whiteBoardData
+                    : fileData?.whiteboard && JSON.parse(fileData?.whiteboard),
+              }}
+              onChange={(excalidrawElements, appState, files) => {
+                if (whiteBoardData?.length > 0) {
+                  return;
+                } else {
+                  setWhiteBoardData(excalidrawElements);
+                }
+              }}
+              UIOptions={{
+                canvasActions: {
+                  changeViewBackgroundColor: true,
+                  saveToActiveFile: true,
+                  loadScene: true,
+                  export: { saveFileToDisk: true },
+                  toggleTheme: true,
+                  saveAsImage: true,
+                },
+              }}
+            >
+              <MainMenu>
+                <MainMenu.DefaultItems.ClearCanvas />
+                <MainMenu.DefaultItems.SaveAsImage />
+                <MainMenu.DefaultItems.ChangeCanvasBackground />
+              </MainMenu>
+              <WelcomeScreen>
+                <WelcomeScreen.Hints.MenuHint />
+                <WelcomeScreen.Hints.MenuHint />
+                <WelcomeScreen.Hints.ToolbarHint />
+                <WelcomeScreen.Center>
+                  <WelcomeScreen.Center.MenuItemHelp />
+                </WelcomeScreen.Center>
+              </WelcomeScreen>
+            </Excalidraw>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
