4747f762638b6b428316d0158a7fab98380c718d
Anish <xevenbiswas@gmail.com>
2024-05-13 13:06:40 +0530

    added contributors page

---
+++ src/app/contributors/ContributorsData.ts
@@ -0,0 +1,19 @@
+export const ContributorsData = [
+  {
+    name: "Anish Biswas",
+    github: "https://git.new/Anish",
+    imageUrl: "https://blastro.netlify.app/assets/anish.webp",
+  },
+  {
+    name: "Subhadeep Roy",
+    github: "https://git.new/subha",
+    imageUrl:
+      "https://media.licdn.com/dms/image/D4D03AQFalsSBySqQCA/profile-displayphoto-shrink_400_400/0/1680632903861?e=1721260800&v=beta&t=EYuh7f1xzFJv4JerrXg9ho_tTaczzkrDik7KuTOMbTE",
+  },
+  {
+    name: "Example Name",
+    github: "https://github.com",
+    imageUrl:
+      "https://assets.lummi.ai/assets/QmU7JUkgkzJuJBFrUkr9phf2biFRV8kR5PLvm3yHZH7GDn?auto=format&w=1500",
+  },
+];
