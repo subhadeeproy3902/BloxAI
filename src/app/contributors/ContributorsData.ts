9362f56bfcd3d7c0b5ae291344a2cc6a36ff150d
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2024-05-13 15:40:48 +0530

    Update ContributorsData.ts

--- src/app/contributors/ContributorsData.ts
+++ src/app/contributors/ContributorsData.ts
@@ -16,4 +16,10 @@ export const ContributorsData = [
     imageUrl:
       "https://github.com/subhadeeproy3902/BloxAI/assets/115932772/8032d384-06d2-44fb-beb4-1227ee035466",
   },
+  {
+    name: "Sarthak Mishra",
+    github: "https://github.com/sarthakmishra459",
+    imageUrl:
+      "https://gcdnb.pbrd.co/images/OHXTn7dGYViJ.jpg?o=1",
+  },
 ];
