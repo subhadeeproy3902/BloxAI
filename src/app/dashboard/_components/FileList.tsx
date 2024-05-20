5a45fa7189fa48d350df4ec9f7e4b031ca02ab97
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-05-21 03:02:05 +0530

    Revert "added sorting options on list header"

--- src/app/dashboard/_components/FileList.tsx
+++ src/app/dashboard/_components/FileList.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Loader2, Trash2, ChevronsUpDown } from "lucide-react";
+import { Loader2, Trash2 } from "lucide-react";
 import moment from "moment";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -36,43 +35,13 @@ function FileList({
   picture: string;
 }) {
   const router = useRouter();
-  const [sortConfig, setSortConfig] = useState<{
-    key: keyof FILE;
-    direction: string;
-  } | null>(null);
-
   const safeFileList = Array.isArray(fileList) ? fileList : [];
 
-  const sortedFiles = [...safeFileList];
-  if (sortConfig !== null) {
-    sortedFiles.sort((a, b) => {
-      if (a[sortConfig.key] < b[sortConfig.key]) {
-        return sortConfig.direction === "ascending" ? -1 : 1;
-      }
-      if (a[sortConfig.key] > b[sortConfig.key]) {
-        return sortConfig.direction === "ascending" ? 1 : -1;
-      }
-      return 0;
-    });
-  }
-
   const deleteFile = useMutation(api.files.deleteFile);
-  const deleteFunc = async (e: any, id: string) => {
+  const deleteFunc = async (e : any, id: string) => {
     e.stopPropagation();
     await deleteFile({ _id: id as Id<"files"> });
     window.location.reload();
-  };
-
-  const requestSort = (key: keyof FILE) => {
-    let direction = "ascending";
-    if (
-      sortConfig &&
-      sortConfig.key === key &&
-      sortConfig.direction === "ascending"
-    ) {
-      direction = "descending";
-    }
-    setSortConfig({ key, direction });
   };
 
   return (
@@ -81,17 +50,11 @@ function FileList({
         <table className="min-w-full divide-y-2 overflow-hidden text-sm">
           <thead className="ltr:text-left rtl:text-right">
             <tr>
-              <td
-                className="whitespace-nowrap px-4 py-2 font-medium cursor-pointer"
-                onClick={() => requestSort("fileName")}
-              >
-                File Name <ChevronsUpDown className="inline-block ml-2" />
+              <td className="whitespace-nowrap px-4 py-2 font-medium">
+                File Name
               </td>
-              <td
-                className="whitespace-nowrap px-4 py-2 font-medium cursor-pointer"
-                onClick={() => requestSort("_creationTime")}
-              >
-                Created At <ChevronsUpDown className="inline-block ml-2" />
+              <td className="whitespace-nowrap px-4 py-2 font-medium">
+                Created At
               </td>
               <td className="whitespace-nowrap px-4 py-2 font-medium">
                 Edited
@@ -105,7 +68,7 @@ function FileList({
             {!fileList && (
               <tr className="relative h-16">
                 <td className="whitespace-nowrap w-full absolute px-4 py-2 mt-5 text-center font-medium flex-center">
-                  <Loader2 className="animate-spin mr-3" size={20} /> Loading... Please wait
+                 <Loader2 className="animate-spin mr-3" size={20}/> Loading... Please wait
                 </td>
               </tr>
             )}
