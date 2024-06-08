4106c477f551262baf948baaa01c647fd99d5618
Sid-80 <siddharthbramhecha@gmail.com>
2024-06-08 12:53:48 +0530

    feat: schema convex

---
+++ convex/schemas.ts
@@ -0,0 +1,24 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  teams: defineTable({
+    
+    teamName: v.string(),
+    createdBy: v.string(),
+    teamMembers: v.array(v.string()),
+  }),
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    image: v.string(),
+  }),
+  files: defineTable({
+    fileName: v.string(),
+    teamId: v.id("teams"),
+    createdBy: v.string(),
+    archive: v.boolean(),
+    document: v.string(),
+    whiteboard: v.string(),
+  }),
+});
