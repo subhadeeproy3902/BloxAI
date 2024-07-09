4fb1ed42138932eb721171ce8ad81af9f6c035b0
Sid-80 <siddharthbramhecha@gmail.com>
2024-07-09 22:17:12 +0530

    feat: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,32 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+import User from "@/models/user";
+import { mongoDB } from "@/lib/MongoDB";
 
-export async function middleware(request: NextRequest) {
+export async function middleware(req: NextRequest) {
+  try {
+    const token : any = req.cookies.get('accessToken') || req.headers.get('Authorization')?.replace('Bearer ', '');
 
+    if (!token) {
+      return NextResponse.json({ error: 'Unauthorized Access!' }, { status: 401 });
+    }
+
+    const decodedToken : any = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string);
+
+    await mongoDB();
+    const user = await User.findById(decodedToken._id).select('-password -refreshToken');
+
+    if (!user) return NextResponse.json({ error: 'Unauthorized Access!' }, { status: 401 });
+
+    // Pass user data in request headers for further processing in API route
+    req.headers.set('user', JSON.stringify(user));
+    
+    return NextResponse.next();
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: 'Error occurred!' }, { status: 500 });
+  }
 }
 
 export const config = {
