ff4d8cbda58ea4a9bcd34639d62258abab7b530d
Mastan Sayyad <sayyadmastan78@gmail.com>
2024-07-10 19:55:38 +0530

    Update ScrollUp.tsx

--- src/components/shared/ScrollUp.tsx
+++ src/components/shared/ScrollUp.tsx
@@ -30,19 +30,17 @@ const ScrollToTopButton = () => {
   };
 
   return (
-    <div
-    className={`fixed bottom-5 right-5 z-50 ${isVisible ? 'block' : 'hidden'}`}
-  >
-    <Button
-      className="scroll-top-button bg-black dark:bg-secondary text-white p-2 rounded-full opacity-75 hover:opacity-100"
-      onClick={goToTop}
-      variant={"secondary"}
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="white" className="arrow">
-        <path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z" />
-      </svg>
-    </Button>
-  </div>
+    <div className={`fixed bottom-5 right-5 z-50 ${isVisible ? 'block' : 'hidden'}`}>
+      <Button
+        className="scroll-top-button bg-black dark:bg-secondary text-white p-2 rounded-full opacity-75 hover:opacity-90 hover:bg-gray-800 dark:hover:bg-gray-700"
+        onClick={goToTop}
+        variant={"secondary"}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="white" className="arrow">
+          <path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z" />
+        </svg>
+      </Button>
+    </div>
   );
 };
 
