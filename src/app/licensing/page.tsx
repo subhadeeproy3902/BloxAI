43fd123df52a26db2af75ba6189562c6f0c25ee0
Mastan Sayyad <sayyadmastan78@gmail.com>
2024-07-10 21:15:16 +0530

    Create page.tsx

---
+++ src/app/licensing/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Licensing from "@/components/shared/licensing";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <Licensing />
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
