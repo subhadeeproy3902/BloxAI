09ba55b20f1cd53d338c8a86761af2ea97a92d68
Sid-80 <siddharthbramhecha@gmail.com>
2024-07-10 19:47:15 +0530

    feat: api for creating new team

---
+++ src/app/api/teams/create/route.ts
@@ -0,0 +1,33 @@
+import { mongoDB } from "@/lib/MongoDB";
+import { AuthMiddleware } from "@/Middleware/AuthMiddleware";
+import TeamModel from "@/models/team";
+import { ApiUser } from "@/types/types";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+
+    const result = await AuthMiddleware(req);
+
+    if (result instanceof NextResponse) {
+        
+        try {
+            const { teamName } = await req.json();
+
+            await mongoDB();
+      
+            const user: ApiUser = JSON.parse(req.headers.get("user") || "{}");
+      
+            const team = await TeamModel.create({
+              teamName,
+              createdBy:user._id,
+              teamMembers:[user._id]
+            });
+      
+            return NextResponse.json({ status: 200 });
+        } catch (err) {
+            return NextResponse.json(`Err : ${err}`, {status:500});
+        }
+    } else {
+      return result;
+    }
+};
