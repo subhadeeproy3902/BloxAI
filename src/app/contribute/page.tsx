90fe88f68001328236c07eac171d8687a1352e09
Radhika Malpani <radhikavishnudas2002@gmail.com>
2024-07-13 17:00:08 +0530

    create page.tsx

---
+++ src/app/contribute/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import ContributingGuidelines from "@/components/shared/contribute";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <ContributingGuidelines />
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
